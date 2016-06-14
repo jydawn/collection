e5dd6a6178c97d6194b91db0843c4dec42b81535
jydawn <1198621249@qq.com>
2016-06-14 18:20:41 +0800

    Update collect_all_even.js

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -2,6 +2,12 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
+  var temp=[];
+    for(var i in collection){
+        if(collection[i]%2 == 0){
+            temp.push(collection[i]);
+        }
+    }
 }
 
 module.exports = collect_all_even;
